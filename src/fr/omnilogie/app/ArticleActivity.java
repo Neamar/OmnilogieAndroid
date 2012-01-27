32c154814326244cbd22924b146792e1a393b0ca
Matthieu Bacconnier <matthieu@bacconnier.fr>
2012-01-27 18:11:46 +0100

    Affiche la liste des sources
    Bug au niveau du défilement vertical...

--- src/fr/omnilogie/app/ArticleActivity.java
+++ src/fr/omnilogie/app/ArticleActivity.java
@@ -4,12 +4,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -22,6 +25,9 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.webkit.WebView;;
 
@@ -32,7 +38,7 @@ public class ArticleActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.article);
 		
-		JSONObject datas = JSONfunctions.getJSONfromURL("http://omnilogie.fr/raw/articles/1205.json");
+		JSONObject datas = JSONfunctions.getJSONfromURL("http://omnilogie.fr/raw/articles/1215.json");
 		try {
 			//Commencer par charger la bannière si nécessaire
 			if(!datas.isNull("Banniere"))
@@ -58,6 +64,9 @@ public class ArticleActivity extends Activity {
 			//Rendre le contenu de l'article.
 			//Il faut spécifier une URL de base afin que les images soient disponibles.
 			((WebView) findViewById(R.id.contenu)).loadDataWithBaseURL("http://omnilogie.fr", preparerArticle(datas.getString("Omnilogisme")), "text/html", "UTF-8", null);
+			
+			//Traiter les sources
+			renderSources(datas.getJSONArray("Sources"));
 		} catch (JSONException e) {
 			// TODO : gérer les erreurs
 			e.printStackTrace();
@@ -71,6 +80,43 @@ public class ArticleActivity extends Activity {
 		return true;
 	}
 	
+	protected void renderSources(JSONArray sources)
+	{
+		ArrayList<HashMap<String, String>> listeSources = new ArrayList<HashMap<String, String>>();
+
+		// Insert les éléments JSON dans listeArticles
+		try {
+			
+			for(int i = 0;i < sources.length();i++)
+			{
+				HashMap<String, String> map = new HashMap<String, String>();	
+				JSONObject e;
+				
+				e = sources.getJSONObject(i);
+				
+				map.put("url", e.getString("URL"));
+				if(e.isNull("Titre"))
+				{
+					map.put("titre", e.getString("URL"));
+				}
+				else
+				{
+					map.put("titre", e.getString("Titre"));
+				}
+				
+				listeSources.add(map);
+			}		
+		} catch (JSONException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		ListAdapter adapter = new SimpleAdapter(this, listeSources , R.layout.sources_item, 
+			new String[] { "titre" }, 
+			new int[] { R.id.sourceItemId });
+		
+		((ListView) findViewById(R.id.sourcesArticle)).setAdapter(adapter);
+	}
 	
 	protected String preparerArticle(String article)
 	{
