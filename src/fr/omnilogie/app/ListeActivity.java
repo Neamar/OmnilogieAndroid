ffe5c2e4d63af1173b50f0b2df3d1d8d78f24f51
Benoit <benoit.maillot@gmail.com>
2012-01-27 15:28:47 +0100

    Récupération du # de l'article après un clic sur la liste

--- src/fr/omnilogie/app/ListeActivity.java
+++ src/fr/omnilogie/app/ListeActivity.java
@@ -8,8 +8,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.ListActivity;
+import android.view.View;
 import android.os.Bundle;
-import android.text.Html;
 import android.util.Log;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -24,6 +24,8 @@ public class ListeActivity extends ListActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	  super.onCreate(savedInstanceState);
+	  
+	  setContentView(R.layout.listes);
 	  
 	  // Tableau qui contiendra les méta-données sur les articles
 	  ArrayList<HashMap<String, String>> listeArticles = new ArrayList<HashMap<String, String>>();
@@ -55,59 +57,18 @@ public class ListeActivity extends ListActivity {
       
       setListAdapter(adapter);
       
+      
+      
       final ListView lv = getListView();
       lv.setTextFilterEnabled(true);
-      /*lv.setOnItemClickListener(new OnItemClickListener() {
+      lv.setOnItemClickListener(new OnItemClickListener() {
       	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {        		
       		@SuppressWarnings("unchecked")
-				HashMap<String, String> o = (HashMap<String, String>) lv.getItemAtPosition(position);	        		
-      		Toast.makeText(Main.this, "ID '" + o.get("id") + "' was clicked.", Toast.LENGTH_SHORT).show(); 
-
+				HashMap<String, String> o = (HashMap<String, String>) lv.getItemAtPosition(position); 
+      			String articleID = o.get("id");
+      			Log.v("todo", "Load article " + articleID);
+      			//TODO charger l'article
 			}
-		});*/
+		});
 	}
-	   
-    static final String[] COUNTRIES = new String[] {
-        "Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra",
-        "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina",
-        "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan",
-        "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium",
-        "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia",
-        "Bosnia and Herzegovina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory",
-        "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi",
-        "Cote d'Ivoire", "Cambodia", "Cameroon", "Canada", "Cape Verde",
-        "Cayman Islands", "Central African Republic", "Chad", "Chile", "China",
-        "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo",
-        "Cook Islands", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic",
-        "Democratic Republic of the Congo", "Denmark", "Djibouti", "Dominica", "Dominican Republic",
-        "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea",
-        "Estonia", "Ethiopia", "Faeroe Islands", "Falkland Islands", "Fiji", "Finland",
-        "Former Yugoslav Republic of Macedonia", "France", "French Guiana", "French Polynesia",
-        "French Southern Territories", "Gabon", "Georgia", "Germany", "Ghana", "Gibraltar",
-        "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau",
-        "Guyana", "Haiti", "Heard Island and McDonald Islands", "Honduras", "Hong Kong", "Hungary",
-        "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica",
-        "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kuwait", "Kyrgyzstan", "Laos",
-        "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg",
-        "Macau", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands",
-        "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia", "Moldova",
-        "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia",
-        "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand",
-        "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "North Korea", "Northern Marianas",
-        "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru",
-        "Philippines", "Pitcairn Islands", "Poland", "Portugal", "Puerto Rico", "Qatar",
-        "Reunion", "Romania", "Russia", "Rwanda", "Sqo Tome and Principe", "Saint Helena",
-        "Saint Kitts and Nevis", "Saint Lucia", "Saint Pierre and Miquelon",
-        "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Saudi Arabia", "Senegal",
-        "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands",
-        "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "South Korea",
-        "Spain", "Sri Lanka", "Sudan", "Suriname", "Svalbard and Jan Mayen", "Swaziland", "Sweden",
-        "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "The Bahamas",
-        "The Gambia", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey",
-        "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Virgin Islands", "Uganda",
-        "Ukraine", "United Arab Emirates", "United Kingdom",
-        "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan",
-        "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Wallis and Futuna", "Western Sahara",
-        "Yemen", "Yugoslavia", "Zambia", "Zimbabwe"
-      };
 }
