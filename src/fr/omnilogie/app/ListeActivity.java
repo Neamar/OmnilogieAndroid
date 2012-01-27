cc552ec418f17e44bd281ff7b3a71b8287a169e0
Matthieu Bacconnier <matthieu@bacconnier.fr>
2012-01-27 11:32:19 +0100

    Ébauche dz l'activité article

--- src/fr/omnilogie/app/ListeActivity.java
+++ src/fr/omnilogie/app/ListeActivity.java
@@ -8,6 +8,6 @@ public class ListeActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.article);
     }
 }
