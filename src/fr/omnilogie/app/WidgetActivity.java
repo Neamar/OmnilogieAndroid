60a75cfadd978d5de989a52e93b1a380758939aa
Benoit <benoit.maillot@gmail.com>
2012-02-08 00:07:10 +0100

    Ajout d'un background potable. Ouverture de l'article quand clic sur le widget.

--- src/fr/omnilogie/app/WidgetActivity.java
+++ src/fr/omnilogie/app/WidgetActivity.java
@@ -12,8 +12,10 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.IBinder;
 import android.text.Html;
 import android.util.Log;
@@ -31,6 +33,7 @@ public class WidgetActivity extends AppWidgetProvider implements CallbackObject 
 	public static Context context;
 	public static AppWidgetManager appWidgetManager;
 	public static int appWidgetIds[];
+	public ArticleObject article;
 	
 	@Override
     public void onUpdate( Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds )    {		
@@ -48,14 +51,44 @@ public class WidgetActivity extends AppWidgetProvider implements CallbackObject 
 		JSONRetriever jsonRetriever = new JSONRetriever();
 		jsonRetriever.getJSONArrayfromURL(url, this);
     }
+
+//	@Override 
+//    public void onEnabled(Context context) {
+//		if(article != null) {
+//			//Intent intent = new Intent(context, UpdateService.class);
+//			Uri uri = Uri.parse("content://fr.omnilogie.app/article/" + article.id);
+//			Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+//			intent.setClassName("fr.omnilogie.app","fr.omnilogie.app.ArticleActivity");
+//			PendingIntent pendingIntent = PendingIntent.getService(context, 0, intent, 0);
+//			
+//			RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.activity_widget);
+//			remoteViews.setOnClickPendingIntent(R.id.layout_widget, pendingIntent);
+//			
+//			remoteViews.setTextViewText(R.id.widget_title, Html.fromHtml(article.titre));
+//			remoteViews.setTextViewText(R.id.widget_subtitle, Html.fromHtml(article.accroche));
+//	
+//			// Tell the widget manager
+//			final int N = appWidgetIds.length;
+//	        for (int i=0; i<N; i++) {
+//	            int appWidgetId = appWidgetIds[i]; 
+//	            appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
+//	        }
+//		}
+//		
+//	}
 	
-	public static class UpdateService extends Service {
+	public class UpdateService extends Service {
         @Override
         public void onStart(Intent intent, int startId) {
-        	WidgetActivity.Widget.onUpdate(context, appWidgetManager, appWidgetIds);
-        	Toast.makeText(context, "Update Widget", Toast.LENGTH_SHORT).show();
+        	//WidgetActivity.Widget.onUpdate(context, appWidgetManager, appWidgetIds);
+        	//Toast.makeText(context, "Update Widget", Toast.LENGTH_SHORT).show();
+//        	if(article != null) {
+//        		Uri uri = Uri.parse("content://fr.omnilogie.app/article/" + article.id);
+//    			Intent i = new Intent(Intent.ACTION_VIEW, uri);
+//    			startActivity(i);
+//        	}
         }
-
+        
 		@Override
 		public IBinder onBind(Intent arg0) {
 			return null;
@@ -71,13 +104,19 @@ public class WidgetActivity extends AppWidgetProvider implements CallbackObject 
 			{
 				// Insert les éléments JSON dans listeArticles
 				try{
-					ArticleObject article = new ArticleObject();
+					article = new ArticleObject();
 					article.remplirDepuisJSON( jsonArray.getJSONObject(0) );
 					
 					//Intent intent = new Intent(context, UpdateService.class);
-					//PendingIntent pendingIntent = PendingIntent.getService(context, 0, intent, 0);
+					Uri uri = Uri.parse("content://fr.omnilogie.app/article/" + article.id);
+	    			Intent intent = new Intent(Intent.ACTION_MAIN, uri);
+	    			intent.addCategory(Intent.CATEGORY_LAUNCHER);
+	    			intent.setComponent(new ComponentName("fr.omnilogie.app","fr.omnilogie.app.ArticleActivity"));
+	    			intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+					PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 					
 					RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.activity_widget);
+					remoteViews.setOnClickPendingIntent(R.id.layout_widget, pendingIntent);
 					
 					remoteViews.setTextViewText(R.id.widget_title, Html.fromHtml(article.titre));
 					remoteViews.setTextViewText(R.id.widget_subtitle, Html.fromHtml(article.accroche));
