7adbc7bd74ca2f8e49ff4c9810db72236883d7fd
Benoit <benoit.maillot@gmail.com>
2012-02-07 22:12:11 +0100

    Mise en forme du widget. Le widget récupère à l'installation sur le desktop les informations sur le dernier article.

--- src/fr/omnilogie/app/WidgetActivity.java
+++ src/fr/omnilogie/app/WidgetActivity.java
@@ -5,6 +5,9 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
 import android.app.PendingIntent;
 import android.app.Service;
 import android.appwidget.AppWidgetManager;
@@ -12,6 +15,8 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
+import android.text.Html;
+import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.Toast;
 
@@ -21,7 +26,7 @@ import android.widget.Toast;
  * @author Benoit
  *
  */
-public class WidgetActivity extends AppWidgetProvider {
+public class WidgetActivity extends AppWidgetProvider implements CallbackObject {
 	public static WidgetActivity Widget = null;
 	public static Context context;
 	public static AppWidgetManager appWidgetManager;
@@ -38,30 +43,10 @@ public class WidgetActivity extends AppWidgetProvider {
 	    WidgetActivity.appWidgetManager = appWidgetManager;
 	    WidgetActivity.appWidgetIds = appWidgetIds;
 	    		
-		final int N = appWidgetIds.length;
-        for (int i=0; i<N; i++) {
-            int appWidgetId = appWidgetIds[i];  
-            
-            updateAppWidget(context,appWidgetManager, appWidgetId);            
-        }
-    }
-	
-	static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-            int appWidgetId) {
-        
-        DateFormat format = SimpleDateFormat.getTimeInstance( SimpleDateFormat.MEDIUM, Locale.getDefault() );
-        CharSequence text = "Time: " + format.format( new Date());
-                
-        Intent intent = new Intent(context, UpdateService.class);
-        PendingIntent pendingIntent = PendingIntent.getService(context, 0, intent, 0);
-        
-        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.activity_widget);
-        remoteViews.setOnClickPendingIntent(R.id.widget_textview, pendingIntent);
-        
-        remoteViews.setTextViewText(R.id.widget_textview, text);
- 
-        // Tell the widget manager
-        appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
+	    String url = "http://omnilogie.fr/raw/articles.json?start=0&limit=1";
+		
+		JSONRetriever jsonRetriever = new JSONRetriever();
+		jsonRetriever.getJSONArrayfromURL(url, this);
     }
 	
 	public static class UpdateService extends Service {
@@ -76,5 +61,40 @@ public class WidgetActivity extends AppWidgetProvider {
 			return null;
 		}
     }
+
+	public void callback(Object objet) {
+		
+		if(objet != null)
+		{
+			JSONArray jsonArray = (JSONArray) objet;
+			if(jsonArray != null)
+			{
+				// Insert les éléments JSON dans listeArticles
+				try{
+					ArticleObject article = new ArticleObject();
+					article.remplirDepuisJSON( jsonArray.getJSONObject(0) );
+					
+					//Intent intent = new Intent(context, UpdateService.class);
+					//PendingIntent pendingIntent = PendingIntent.getService(context, 0, intent, 0);
+					
+					RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.activity_widget);
+					
+					remoteViews.setTextViewText(R.id.widget_title, Html.fromHtml(article.titre));
+					remoteViews.setTextViewText(R.id.widget_subtitle, Html.fromHtml(article.accroche));
+
+					// Tell the widget manager
+					final int N = appWidgetIds.length;
+			        for (int i=0; i<N; i++) {
+			            int appWidgetId = appWidgetIds[i]; 
+			            appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
+			        }
+						
+				}catch(JSONException e) {
+					Log.e("log_tag", "Error parsing data "+e.toString());
+				}
+				
+			}
+		}		
+	}
 	
 }
