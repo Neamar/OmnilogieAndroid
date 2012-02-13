ffe4ffc00318233ea2175f3e76e92da5343ef131
Neamar <neamar@neamar.fr>
2012-02-13 19:30:56 +0100

    Correction d'un bug affectant les articles finissant par un point d'interrogation

--- src/fr/omnilogie/app/ArticleActivity.java
+++ src/fr/omnilogie/app/ArticleActivity.java
@@ -2,6 +2,7 @@
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URLEncoder;
 
 import org.json.JSONObject;
 
@@ -9,6 +10,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -45,7 +47,7 @@ public class ArticleActivity extends DefaultActivity implements CallbackObject {
 			//Il suffit de lire l'URI avec lequel cette activité a été appelée
 			Uri uri = getIntent().getData();
 			String articleToDisplay = uri.getLastPathSegment();
-			
+			Log.e("wtf", articleToDisplay);
 			//Télécharge le contenu de l'article de manière asynchrone.
 			//La méthode callback est appelée après la récupération.
 			JSONRetriever jsonRetriver = new JSONRetriever();
